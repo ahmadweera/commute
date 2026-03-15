4da367603b9b09f2fe1a305b1c1ef31fe779e709
Ahmad Weera <ahmadt.weera@gmail.com>
2026-03-15 14:34:26 -0400

    Add custom Vite plugin to copy index.html to 404.html on build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { copyFileSync } from 'fs'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   base: '/commute/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      name: 'copy-404',
+      closeBundle() {
+        copyFileSync(join(__dirname, 'dist/index.html'), join(__dirname, 'dist/404.html'))
+      },
+    },
+  ],
 })
